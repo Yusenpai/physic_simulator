80b33f616f93e66b93f58fc7e623f7d80ece23e0
Yusenpai <humxam2712@gmail.com>
2025-03-08 22:06:29 +0700

    remove build folder

--- build/CMakeFiles/FluidSimulator.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for FluidSimulator.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/FluidSimulator.dir/src/grid.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/FluidSimulator.dir/src/grid.cpp.o: \
-  /Users/yusenpai/fluid\ simulator/src/grid.cpp \
-  /Users/yusenpai/fluid\ simulator/src/grid.hpp
